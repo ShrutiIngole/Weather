20a685c440b9f7f6c07d0ee2091e585a095e4c7c
ShrutiIngole <ingoleshruti11@gmail.com>
2020-11-26 19:58:09 +0530

    search component update

--- src/components/search.js
+++ src/components/search.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import axios from 'axios';
 
+import store from '../redux/store'
 const SearchBar = () => {
     const handleClick = (e) => {
-        console.log(document.getElementById('city').value)
+        var city = document.getElementById('city').value
+        store.dispatch({
+            type: "CITY",
+            payload: {
+                city: city
+            }
+        })
+        console.log(store.getState())
+        getWeather(city)
+        console.log(store.getState())
     }
+
+    const getWeather = (city) => {
+        const current = `http://api.weatherapi.com/v1/current.json?key=d6e8d08550c3413cb08105419202011&q=${city}`
+        axios.get(current)
+          .then(res=> {
+            store.dispatch({
+              type: "WEATHER",
+              payload: {
+                city: res.data.location.name,
+                lat: res.data.location.lat,
+                long: res.data.location.lon,
+                condition: res.data.current.condition.text,            
+                temp: res.data.current.temp_c,
+                imgSrc: res.data.current.condition.icon,
+                conCode: res.data.current.condition.code
+              }
+            })
+          })
+      }
     return(
         <span id="searchbar">
             <input type="text" placeholder="Enter City" id="city" /><button onClick={e=>handleClick(e)}>Search</button>
